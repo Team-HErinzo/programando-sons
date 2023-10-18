0d9d344b9149832851d95124c1620889f81101ff
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2023-10-18 12:08:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
+    const instrumento = /].classList[1];
     const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
@@ -16,3 +16,4 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
 }
+  JSON
