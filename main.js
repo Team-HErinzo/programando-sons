d54193fa7a5d83d81919320c0b4945efdaa68796
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2023-12-13 04:23:58 +0000

    Terminando o site

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+const listaDeTeclas = document.querySelectorAll ('.tecla');
 
+let contador = 0;
+
+
+while (contador < 9) {
+    
     const tecla = listaDeTeclas[contador];
-    const instrumento = /].classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
+    contador = contador + 1;
+
+    //console.log(contador);
 }
-  JSON
